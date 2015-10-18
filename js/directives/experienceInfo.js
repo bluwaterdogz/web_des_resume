d492a3307f0e73aa78f65715f39d66a532bf3420
Bluwaterdogz <bveedy@gmail.com>
2015-10-17 17:35:16 -0700

    revert for now

--- js/directives/experienceInfo.js
+++ js/directives/experienceInfo.js
@@ -1,37 +1,27 @@
 app.directive('expInfo', [function() {
   return {
-    
     restrict: 'E',
-   
     scope: {
       info: '='
     },
-    
     templateUrl: 'js/directives/experienceInfo.html',
-
-	link: function(scope){
-
-		scope.unhidden = true;
-
-		scope.anim = function(){
-
-			if(scope.hidden){
-				$('.expbox').css({
-					height: 'auto',
-					overflow: 'visible'
-				});
-				scope.hidden = false;
-			}
-			else{
-				$('.expbox').css({
-					height: 'auto',
-					overflow: 'visible'
-				});
-				scope.hidden = true;
-
-			}
-
-		}
-	}
+    link: function(scope) {
+      scope.unhidden = true;
+      scope.anim = function() {
+        if (scope.hidden) {
+          $('.expbox').css({
+            height: 'auto',
+            overflow: 'visible'
+          });
+          scope.hidden = false;
+        } else {
+          $('.expbox').css({
+            height: 'auto',
+            overflow: 'visible'
+          });
+          scope.hidden = true;
+        }
+      }
+    }
   }
 }]);
--- js/services/classesService.js
+++ js/services/classesService.js
@@ -1,6 +1,47 @@
 app.factory('ClassesService', ['$resource' function ($resource) {
 
-	var classes = $resource("json/classes.json");
+	var classes = [
+  {
+    title:"Web Applications",
+    date:"Fall 2015",
+    description:"Web Application development using Web2Py and angularJS.",
+    skill:"❤❤❤"
+  },
+  {
+    title:"WebPublishing 1 & 2",
+    date:"Spring 2015",
+    description:"First course in web publishing series based on HTML and CSS",
+    skill:"❤❤❤"
+  },
+  {
+    title:"Javascript",
+    date:"Winter 2015",
+    description:"Introduction to browser based Javascript.",
+    skill:"❤❤❤"
+  },
+  {
+    title:"Mobile Programming: IOS and Android",
+    date:"Spring 2015",
+    skill:"❤",
+    description:"Mobile app development using Android studio and Xcode with either Java or Swift Programming languages."
+  },
+  {
+    title:"Programming Database Driven Websites with PHP",
+    date:"Spring 2015",
+    description:"Introduction to PHP server-side programming language as an OOP and the implementation of SQL into websites programmed with it.",
+    skill:"❤❤❤"
+  },
+  {
+    title:"Java programming",
+    date:"Fall 2015",
+    description:"Introduction to OOP Java using the Greenfoot programming library, followed by an intermediate Java class.",
+    skill:"❤❤❤"
+
+  }
+];
+
+
+	// $resource("json/classes.json").get();
 
 	return {
 
