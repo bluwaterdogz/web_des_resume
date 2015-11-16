ff490f80da32bd60adc54046c3c7ec3d4b8c9f25
Bluwaterdogz <bveedy@gmail.com>
2015-11-16 10:26:12 -0800

    update
    class misnamed

--- js/directives/skillsInfo.js
+++ js/directives/skillsInfo.js
@@ -1,20 +1,19 @@
-app.directive('skillsInfo', function() { 
-  return { 
-    restrict: 'E', 
-    scope: { 
-      info: '=' 
-    }, 
+app.directive('skillsInfo', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      info: '='
+    },
     templateUrl: 'js/directives/skillsInfo.html',
 
     link: function(scope){
-
       scope.likes = 0;
 
       scope.addLike = function(){
         scope.likes++;
     }
-    
+
     }
-    
-  }; 
+
+  };
 });
