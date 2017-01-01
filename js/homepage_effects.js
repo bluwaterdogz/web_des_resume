5b196acdf840d0de6414cd7666e8b008f7663d4d
PS <ps@macbooks-MacBook-Pro.local>
2017-01-01 12:55:35 -0800

    polish

--- js/homepage_effects.js
+++ js/homepage_effects.js
@@ -1,41 +1,15 @@
- $(document).ready(function($) {
+$(document).ready(function($) {
 
 	$(".project-title-cont li").mouseenter(function() {
 		var newImg = $(this).index();
     console.log(newImg);
 		$(".img-over").addClass("transparent");
 		$(".img-over").eq(newImg).removeClass("transparent");
-
-	});
-
-	// Dancin Divs
-	$('.sway').each(function(){
-
-		var $this = $(this);
-		setInterval(function(){
-			var pos = newPosition(),
-				timeChange = Math.floor(Math.random()*1000)+1000;
-
-			$this.animate({
-				top:pos[0],
-				left:pos[1]
-			}, timeChange, "swing");
-
-		},400);
 	});
 
 	$('a.backToTop').click(function (e) {
-  e.preventDefault();
-  $(document.body).animate({scrollTop: 0}, 2000, 'swing');
-});
+    e.preventDefault();
+    $(document.body).animate({scrollTop: 0}, 2000, 'swing');
+  });
 
 });
-
-// Calculates positiond for dancing divs
-function newPosition(){
-	var RANGE = 15,
-		halfRange = Math.floor(RANGE/2),
-		x = Math.floor(Math.random()*RANGE-halfRange),
-		y = Math.floor(Math.random()*RANGE-halfRange);
-	return [x,y];
-}
--- js/routes/config.js
+++ js/routes/config.js
@@ -1,30 +1,64 @@
+// app.config(function($stateProvider, $urlRouterProvider){
+//   $urlRouterProvider.otherwise('/');
+//   $stateProvider
+//     .state('intro',{
+//       url:"/intro",
+//       templateUrl:'/web_des_resume/views/intro.html'
+//     })
+//     .state('splash',{
+//       url:"/",
+//       templateUrl:'/web_des_resume/views/splash.html',
+//       controller:'SplashCtrl'
+//     })
+//     .state('skills',{
+//         url:"/skills",
+//         templateUrl:'/web_des_resume/views/skills.html'
+//     })
+//     .state('about',{
+//         url:"/about",
+//         templateUrl:'/web_des_resume/views/about.html'
+//     })
+//     .state('experiences',{
+//         url: "/experiences",
+//         views: {
+//             '': { templateUrl:'/web_des_resume/views/experiences.html'},
+//             'work@experiences': { templateUrl: '/web_des_resume/views/work.html'},
+//             'projects@experiences': {
+//                templateUrl: '/web_des_resume/views/projects.html',
+//                controller:'ProjectCtrl'
+//              }
+//         }
+//     });
+// });
+
+
 app.config(function($stateProvider, $urlRouterProvider){
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise("/");
   $stateProvider
     .state('intro',{
       url:"/intro",
-      templateUrl:'/web_des_resume/views/intro.html'
+      templateUrl:'/views/intro.html'
     })
     .state('splash',{
       url:"/",
-      templateUrl:'/web_des_resume/views/splash.html',
+      templateUrl:'/views/intro.html',
       controller:'SplashCtrl'
     })
     .state('skills',{
         url:"/skills",
-        templateUrl:'/web_des_resume/views/skills.html'
+        templateUrl:'/views/skills.html'
     })
     .state('about',{
         url:"/about",
-        templateUrl:'/web_des_resume/views/about.html'
+        templateUrl:'/views/about.html'
     })
     .state('experiences',{
         url: "/experiences",
         views: {
-            '': { templateUrl:'/web_des_resume/views/experiences.html'},
-            'work@experiences': { templateUrl: '/web_des_resume/views/work.html'},
+            '': { templateUrl:'/views/experiences.html'},
+            'work@experiences': { templateUrl: '/views/work.html'},
             'projects@experiences': {
-               templateUrl: '/web_des_resume/views/projects.html',
+               templateUrl: '/views/projects.html',
                controller:'ProjectCtrl'
              }
         }
